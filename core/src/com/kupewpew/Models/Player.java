f22c0739a052483547a3988dbb16ad01ce756d92
thanawit <thanawit.g@ku.th>
2016-05-30 14:46:34 +0700

    Access menu

--- core/src/com/kupewpew/Models/Player.java
+++ core/src/com/kupewpew/Models/Player.java
@@ -22,8 +22,8 @@ public class Player {
     }
 
     public static Player getInstance() {
-        if(player == null) player = new Player();
-        return player;
+        return player = new Player();
+        //return player;
     }
 
     private Player(String name) {
@@ -46,9 +46,12 @@ public class Player {
 
     public void reset() {
         this.live = 3;
-        this.HP = 0;
         this.score = 0;
-        this.bomb.clear();
+        this.HP = 3;
+        this.bomb = new Stack<Integer>();
+        this.pX = 0;
+        this.pY = 0;
+        this.isInvulnerable = false;
     }
     public boolean isInvulnerable() {
         return isInvulnerable;
